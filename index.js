736970111ccc4b44501fd94f41e18cb8710ea228
S4lvious <51218675+S4lvious@users.noreply.github.com>
2023-10-19 02:08:09 +0200

    First commit!

---
+++ index.js
@@ -0,0 +1,39 @@
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+let cocktail = "";
+const body = document.querySelector("body");
+
+
+function setCocktail(event) {
+    cocktail = event.target.value;
+    getCocktailData();
+}
+
+function getCocktailData () {
+    fetch(url + cocktail).then((response) =>{
+        response.json().then((data) =>{
+            if(data.drinks.length > 0) {
+            const oldResults = document.querySelectorAll(".result");
+            if(oldResults.length > 0) {
+                for (let i = 0; i<oldResults.length; i++){
+                    oldResults[i].remove();
+                }
+
+            }
+            for(let i = 0; i<data.drinks.length; i++) {
+                const titleDrink = document.createElement("h1");
+                titleDrink.className = "result";
+                const img = document.createElement("img");
+                img.className = "result";
+                const paragraph = document.createElement("p");
+                paragraph.className = "result";
+                titleDrink.innerText = data.drinks[i].strDrink;
+                paragraph.innerText = data.drinks[i].strInstructionsIT;
+                img.src = data.drinks[i].strDrinkThumb;
+                body.appendChild(titleDrink);
+                body.appendChild(paragraph);
+                body.appendChild(img);
+            }
+        }
+        })
+    })
+}
